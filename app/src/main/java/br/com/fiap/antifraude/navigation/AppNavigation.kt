8a5ff4963ebb20b5457c1b9126f4a5020dfd8f76
GuilhermeSilva <guilherme.silva@klab.com.br>
2024-11-30 20:51:07 -0300

    feat: navegacao entre telas

--- app/src/main/java/br/com/fiap/antifraude/navigation/AppNavigation.kt
+++ app/src/main/java/br/com/fiap/antifraude/navigation/AppNavigation.kt
@@ -4,6 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import br.com.fiap.antifraude.screens.DocumentsScreen
+import br.com.fiap.antifraude.screens.FacialScreen
+import br.com.fiap.antifraude.screens.FingerPrintScreen
+import br.com.fiap.antifraude.screens.HomeScreen
+import br.com.fiap.antifraude.screens.ScoreScreen
 import br.com.fiap.antifraude.screens.TaskGridScreen
 
 @Composable
@@ -12,10 +17,25 @@ fun AppNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = "taskGrid"
+        startDestination = "home",
     ) {
+        composable("home") {
+            HomeScreen(navController)
+        }
         composable("taskGrid") {
-            TaskGridScreen(navController = navController)
+            TaskGridScreen(navController)
+        }
+        composable("fingerprint") {
+            FingerPrintScreen(navController)
+        }
+        composable("facial") {
+            FacialScreen(navController)
+        }
+        composable("documents") {
+            DocumentsScreen(navController)
+        }
+        composable("score") {
+            ScoreScreen(navController)
         }
     }
 }
